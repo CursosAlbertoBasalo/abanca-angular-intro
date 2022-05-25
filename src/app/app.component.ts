67680d9060a7d4caa910804d8ed66210d3c5e5c4
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-25 11:09:37 +0200

    feat: agenies list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Astro Bookings';
   subtitle = 'Welcome on board';
+  agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ];
 }
