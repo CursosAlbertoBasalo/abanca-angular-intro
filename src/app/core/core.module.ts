cf5c741746b19eb7cbf5f2419aa543838c101c1f
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-27 11:13:03 +0200

    feat: header component

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent],
+})
+export class CoreModule {}
