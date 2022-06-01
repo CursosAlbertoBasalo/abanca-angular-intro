25fa1b6ef6afee9038748b5e7379a7a835b6f251
Alberto Basalo <albertobasalo@hotmail.com>
2022-06-01 10:51:31 +0200

    feat: validators

--- src/app/contact/contact.form.ts
+++ src/app/contact/contact.form.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -11,15 +17,44 @@ export class ContactForm implements OnInit {
 
   constructor(formBuilder: FormBuilder) {
     this.form = formBuilder.group({
-      name: new FormControl(''),
-      email: new FormControl(''),
-      message: new FormControl(''),
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('Call me back'),
     });
   }
 
-  onSubmit(): void {
+  public hasError(controlName: string): boolean {
+    const control = this.getControl(controlName);
+    if (!control) return false;
+    return control.invalid;
+  }
+
+  public mustShowMessage(controlName: string): boolean {
+    const control = this.getControl(controlName);
+    if (!control) return false;
+    return control.touched && control.invalid;
+  }
+
+  public getErrorMessage(controlName: string): string {
+    const control = this.getControl(controlName);
+    if (!control) return '';
+    if (!control.errors) return '';
+    const errors = control.errors;
+    let errorMessage = '';
+    errorMessage += errors['required'] ? '🔥 Field is required ' : ' ';
+    errorMessage += errors['email'] ? '🔥 Should be an email address ' : ' ';
+    errorMessage += errors['minlength']
+      ? `🔥 More than ${errors['minlength'].requiredLength} chars`
+      : ' ';
+    return errorMessage;
+  }
+
+  public onSubmit(): void {
     const contact = this.form.value;
     console.warn('Send contact message', contact);
+  }
+  private getControl(controlName: string): AbstractControl | null {
+    return this.form.get(controlName);
   }
 
   ngOnInit(): void {}
